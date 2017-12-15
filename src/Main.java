585fe6c922c88d35a3525f3fae1192cb7184d601
20180440 <20180440@archwood.org>
2017-12-15 08:19:33 -0500

    Just getting started

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+
+
 import javax.swing.*;
 
 public class Main {
-    static void Main (String[]args){
+   public static void main (String[]args){
         //Determine if latin or english
         //Translate
-        
+        JOptionPane.showConfirmDialog(null, "Translate to PigLatin?" ,"Translator", JOptionPane.YES_NO_OPTION);
+        if (==JOptionPane.NO_OPTION){JOptionPane.showConfirmDialog(null,"Translate to English","Translator",JOptionPane.YES_NO_OPTION);}
+
 
     }
 }
