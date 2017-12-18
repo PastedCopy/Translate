18fc2c60dbf713b1481dc5b41085fe534c8ce05c
20180440 <20180440@archwood.org>
2017-12-18 08:26:42 -0500

    Close but trouble still inhibits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 
-
 import javax.swing.*;
 
 public class Main {
@@ -7,8 +6,14 @@ public class Main {
         //Determine if latin or english
         //Translate
         JOptionPane.showConfirmDialog(null, "Translate English to PigLatin?" ,"Translator", JOptionPane.YES_NO_OPTION);
-        if (1==JOptionPane.NO_OPTION)
-        { JOptionPane.showConfirmDialog(null,"Translate PigLatin to English","Translator",JOptionPane.YES_NO_OPTION);}
-        if (0==JOptionPane.YES_OPTION) {JOptionPane.showInputDialog("Input your sentence.");}
-    }
+
+        if (1==JOptionPane.NO_OPTION) {
+            JOptionPane.showConfirmDialog(null,"Translate PigLatin to English","Translator",JOptionPane.YES_NO_OPTION);
+        if(1==JOptionPane.NO_OPTION){}}
+
+        if (0==JOptionPane.YES_OPTION) {
+            Pig;
+        }
+
+   }
 }
